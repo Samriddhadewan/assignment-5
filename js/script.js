3538d21f868be5036ac78fdd4f653538cae803b0
Samriddha-Dewan <dewansamriddha@gmail.com>
2024-12-11 15:22:32 +0600

    calculations done

--- js/script.js
+++ js/script.js
@@ -8,10 +8,31 @@ document.getElementById('donate-noakhali').addEventListener('click', function(){
 
     setInnerText('noakhali-amount', NewDeposit);
     setInnerText('main-balance', debited);
+
+    document.getElementById('noakhali-input').value = "";
 })
 document.getElementById('donate-feni').addEventListener('click', function(){
-    console.log('noakhali Clicked');
+    const donateValue = getNumberFromInput('feni-input');
+    const mainBalance = getNumberFromStr('main-balance');
+    const deposit = getNumberFromStr('feni-amount');
+
+    const debited = mainBalance - donateValue;
+    const NewDeposit = deposit + donateValue;
+
+    setInnerText('feni-amount', NewDeposit);
+    setInnerText('main-balance', debited);
+    document.getElementById('feni-input').value = "";
 })
 document.getElementById('donate-injury').addEventListener('click', function(){
-    console.log('noakhali Clicked');
+    const donateValue = getNumberFromInput('injury-input');
+    const mainBalance = getNumberFromStr('main-balance');
+    const deposit = getNumberFromStr('injuy-amount');
+
+    const debited = mainBalance - donateValue;
+    const NewDeposit = deposit + donateValue;
+
+    setInnerText('injuy-amount', NewDeposit);
+    setInnerText('main-balance', debited);
+    document.getElementById('injury-input').value = "";
+
 })
