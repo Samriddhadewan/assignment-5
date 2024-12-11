223fe12b8db7ec68126cf9be998c6eb0c67c887c
Samriddha-Dewan <dewansamriddha@gmail.com>
2024-12-11 18:23:38 +0600

    70% done

--- js/toggle.js
+++ js/toggle.js
@@ -1,7 +1,20 @@
 document.getElementById('transition-section-btn').addEventListener('click', function(){
-    showSectionByIdName('transition-section')
+    showSectionByIdName('transition-section');
+    document.getElementById('donation-section-btn').classList.remove('bg-[#B4F461]')
+    document.getElementById('transition-section-btn').classList.add('bg-[#B4F461]')
+    document.getElementById('transition-section-btn').classList.add('font-semibold');
+    document.getElementById('transition-section-btn').classList.add('text-[#111]');
+    document.getElementById('donation-section-btn').classList.add('font-normal')
+    document.getElementById('donation-section-btn').classList.add('#111111B2');
+    document.getElementById('donation-section-btn').classList.add('border');
+    document.getElementById('transition-section-btn').classList.remove('font-normal');
+    
 })
 document.getElementById('donation-section-btn').addEventListener('click', function(){
-    showSectionByIdName('donation-section')
+    showSectionByIdName('donation-section');
+    document.getElementById('donation-section-btn').classList.add('bg-[#B4F461]')
+    document.getElementById('transition-section-btn').classList.remove('bg-[#B4F461]');
+    document.getElementById('transition-section-btn').classList.remove('font-bolder');
+
 })
 
