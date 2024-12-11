e6c950327bf7801d2b012f82a8c116e411e3112c
Samriddha-Dewan <dewansamriddha@gmail.com>
2024-12-11 13:46:00 +0600

    done first thing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 document.getElementById('donate-noakhali').addEventListener('click', function(){
     const donateValue = getNumberFromInput('noakhali-input');
     const mainBalance = getNumberFromStr('main-balance');
+    const deposit = getNumberFromStr('noakhali-amount');
 
-    dabited
+    const debited = mainBalance - donateValue;
+    const NewDeposit = deposit + donateValue;
+
+    setInnerText('noakhali-amount', NewDeposit);
+    setInnerText('main-balance', debited);
 })
 document.getElementById('donate-feni').addEventListener('click', function(){
     console.log('noakhali Clicked');
